18f7d9b4e5c507ceaa90d27d34feb63e63993768
BudCowmen <budcowmen@gmail.com>
2024-11-04 20:53:54 +0900

    Update ApartmentCalculator.jsx

--- src/app/src/components/ApartmentCalculator.jsx
+++ src/app/src/components/ApartmentCalculator.jsx
@@ -19,3 +19,36 @@ const ApartmentCalculator = () => {
     propertyManagement: 8,
     annualAppreciation: 3,
   });
+  const calculateResults = () => {
+    // Loan calculations
+    const loanAmount = inputs.purchasePrice * (1 - inputs.downPayment / 100);
+    const monthlyInterest = inputs.interestRate / 12 / 100;
+    const numberOfPayments = inputs.loanTerm * 12;
+    const monthlyMortgage = loanAmount * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / 
+                           (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+    // Annual income and expenses
+    const annualGrossIncome = inputs.grossMonthlyRent * 12;
+    const vacancyLoss = annualGrossIncome * (inputs.vacancy / 100);
+    const effectiveGrossIncome = annualGrossIncome - vacancyLoss;
+    
+    const annualExpenses = {
+      mortgage: monthlyMortgage * 12,
+      propertyTax: inputs.propertyTax,
+      insurance: inputs.insurance,
+      maintenance: inputs.maintenance,
+      utilities: inputs.utilities,
+      propertyManagement: effectiveGrossIncome * (inputs.propertyManagement / 100)
+    };
+
+    const totalExpenses = Object.values(annualExpenses).reduce((a, b) => a + b, 0);
+    const netOperatingIncome = effectiveGrossIncome - totalExpenses + annualExpenses.mortgage;
+    const cashFlow = effectiveGrossIncome - totalExpenses;
+    
+    // Return metrics
+    const capRate = (netOperatingIncome / inputs.purchasePrice) * 100;
+    const cashOnCashReturn = (cashFlow / (inputs.purchasePrice * inputs.downPayment / 100)) * 100;
+    
+    return {
+      monthlyMortgage,
+      effectiveGrossIncome
