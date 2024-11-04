f105be6233dfc4cb9254e45c9286178b806c7992
BudCowmen <budcowmen@gmail.com>
2024-11-04 20:51:55 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,9 @@
+import ApartmentCalculator from '@/components/ApartmentCalculator'
 
+export default function Home() {
+  return (
+    <main className="container mx-auto py-10">
+      <ApartmentCalculator />
+    </main>
+  )
+}
